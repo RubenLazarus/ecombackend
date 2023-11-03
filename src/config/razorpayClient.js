3d9c3a1f187aab7982d4cc1d8fed139252c409de
RubenLazarus <rlazarus@moreyeahs.in>
2023-11-03 17:23:52 +0530

    change razorpay cred

--- src/config/razorpayClient.js
+++ src/config/razorpayClient.js
@@ -1,7 +1,7 @@
 const Razorpay = require('razorpay');
 
-apiKey="rzp_test_V58aNPoa7wSiXe"
-apiSecret="T4g8gDgWrWxmflBNfFB1TDdq"
+apiKey="rzp_test_MCCNl29eR7GriW"
+apiSecret="cWBHJx30jaTlOlvpNLF4CUtI"
 
 const razorpay = new Razorpay({
     key_id: apiKey,
