5100a2cbdbe193ad284ef79a356c51c844c27952
RubenLazarus <rubenlazarus19@gmail.com>
2023-12-03 22:28:30 +0530

    get product by id

--- src/routes/product.admin.routes.js
+++ src/routes/product.admin.routes.js
@@ -7,5 +7,6 @@ router.post('/', productController.createProduct);
 router.post('/creates', productController.createMultipleProduct);
 router.delete('/:id', productController.deleteProduct);
 router.put('/:id', productController.updateProduct);
+router.get('/:id', productController.findProductById);
 
 module.exports=router;
