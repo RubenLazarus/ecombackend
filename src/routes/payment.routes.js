5b153cd4bb5d389b4971cc9a45041ada38b3ada2
RubenLazarus <rubenlazarus19@gmail.com>
2024-01-20 21:25:00 +0530

    pay later

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,7 +3,7 @@ const authenticate = require("../middleware/authenticat.js");
 const router=express.Router();
 const paymentController=require("../controllers/payment.controller.js");
 
-router.post("/:id",authenticate,paymentController.createPaymentLink);
+// router.post("/:id",authenticate,paymentController.createPaymentLink);
 router.post("/",authenticate,paymentController.updatePaymentInformation);
 router.post("/paylater",authenticate,paymentController.paylater);
 router.post("/paytm",authenticate,paymentController.paytm);
