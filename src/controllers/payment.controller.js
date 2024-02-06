367c89e1eaf8390353748bb76ba3ec5c6db8d472
RubenLazarus <rubenlazarus19@gmail.com>
2024-02-06 22:37:41 +0530

    paytm

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,5 +1,7 @@
 const paymentService = require("../services/payment.service.js")
 const PaytmChecksum = require('paytmchecksum')
+const crypto =  require('crypto');
+const axios = require('axios');
 const https = require('https');
 var mid = 'ffyvpw67039918604874'
 var mKey = 'yWtPp&P4Gqh!IPTr'
@@ -166,6 +168,94 @@ const paytm2 = async (req,res)=>{
         post_req.end();
     });
 }
+const newPayment = async (req, res) => {
+    try {
+        const merchantTransactionId = req.body.transactionId;
+        const data = {
+            merchantId: process.env.MERCHANT_ID,
+            merchantTransactionId: merchantTransactionId,
+            merchantUserId: req.body.MUID,
+            name: req.body.name,
+            amount: req.body.amount * 100,
+            redirectUrl: `https://ecombackend-dgdu.onrender.com/api/payments/status/${merchantTransactionId}`,
+            redirectMode: 'POST',
+            mobileNumber: req.body.number,
+            paymentInstrument: {
+                type: 'PAY_PAGE'
+            }
+        };
+        const payload = JSON.stringify(data);
+        const payloadMain = Buffer.from(payload).toString('base64');
+        const keyIndex = 1;
+        const string = payloadMain + '/pg/v1/pay' + process.env.SALTKEY;
+        const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+        const checksum = sha256 + '###' + keyIndex;
+
+        // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+        const sendbox_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+        const options = {
+            method: 'POST',
+            url: sendbox_URL,
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-VERIFY': checksum
+            },
+            data: {
+                request: payloadMain
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            console.log(response.data)
+            return res.status(200).send(response.data)
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+const checkStatus = async(req, res) => {
+    const merchantTransactionId = req.params['txnId']
+    const merchantId = process.env.MERCHANT_ID
+
+    const keyIndex = 1;
+    const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + process.env.SALTKEY;
+    const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+    const checksum = sha256 + "###" + keyIndex;
+
+    const options = {
+    method: 'GET',
+    url: `https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/${merchantId}/${merchantTransactionId}`,
+    headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-VERIFY': checksum,
+        'X-MERCHANT-ID': `${merchantId}`
+    }
+    };
+
+    // CHECK PAYMENT TATUS
+    axios.request(options).then(async(response) => {
+        if (response.data.success === true) {
+            const url = `https://ecomfrontend-snowy.vercel.app/payment/success`
+            return res.redirect(url)
+        } else {
+            const url = `https://ecomfrontend-snowy.vercel.app/payment/failed`
+            return res.redirect(url)
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+};
 
 
-module.exports = { createPaymentLink, updatePaymentInformation, paylater, paytm, paytmCallback,paytm2 }
+module.exports = { createPaymentLink, updatePaymentInformation, paylater, paytm, paytmCallback,paytm2 ,newPayment,checkStatus}
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -8,6 +8,7 @@ router.post("/",authenticate,paymentController.updatePaymentInformation);
 router.post("/paylater",authenticate,paymentController.paylater);
 router.post("/paytm",paymentController.paytm);
 router.post("/callback",authenticate,paymentController.paytmCallback);
-
+router.post("/phonepe",authenticate,paymentController.newPayment);
+router.post('/status/:txnId', paymentController.checkStatus);
 
 module.exports=router;
