e43937229bbd233b502dec65ccc0ab0d27c92624
RubenLazarus <rubenlazarus19@gmail.com>
2024-02-25 14:28:33 +0530

    nodemailer username and pass chanegs

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -7,20 +7,20 @@ const template = require('./templates.service')
 const transporter = nodemailer.createTransport({
     service: 'Gmail', // Change this to your email service provider (e.g., Outlook, SendGrid, etc.)
     auth: {
-      user: 'rubenlazarus19@gmail.com', // Replace with your email address
-      pass: 'wnkybtrwkgcirikr', // Replace with your email password or an app password
+      user: 'ravi.softsolutions@gmail.com', // Replace with your email address
+      pass: 'jnjdgjstlrdgahcm', // Replace with your email password or an app password
     },
     secure:false,
     requireTLS: false,
     tls: {rejectUnauthorized: false},
-    default:{ from: 'Vidhi Rice <rubenlazarus19@gmail.com>',}
+    default:{ from: 'Vidhi Rice <ravi.softsolutions@gmail.com>',}
   });
 
 
   async function sendOTP(otp,userInfo){
     try {
         return await transporter.sendMail({
-            from: 'Vidhi Rice <rubenlazarus19@gmail.com>',
+            from: 'Vidhi Rice <ravi.softsolutions@gmail.com>',
             to:userInfo.email,
             subject:"Your OTP Code",
             html: await template.forgotpasswortTemp(otp,userInfo),
